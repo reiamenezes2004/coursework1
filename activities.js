b8b14bbfbcc9f54a05cc77f3ec66602ec94bd8c1
Reia Menezes <reiamenezes04@gmail.com>
2023-11-19 13:09:26 +0400

    added all the products to the activities js file.

---
+++ activities.js
@@ -0,0 +1,93 @@
+let activities = [
+    {
+        "id":1001,
+        "title": "Art",
+        "location": "Location: Mirdif",
+        "price": 20.00,
+        "image": "images/art.jpg",
+        "availableInventory": 5,
+        "rating": 5
+    },
+    {
+        "id":1002,
+        "title": "Chess",
+        "location": "Location: Al Nahda",
+        "price": 15.00,
+        "image":"images/chess.png",
+        "availableInventory": 5,
+        "rating": 4 
+    },
+    {
+        "id":1003,
+        "title": "Cricket",
+        "location": "Location: Al Quoz",
+        "price": 30.00,
+        "image":"images/cricket.jpg",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id":1004,
+        "title": "Dance",
+        "location": "Location: Al Khail",
+        "price": 25.00,
+        "image":"images/dance.jpg",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id":1005,
+        "title": "Gymnastics",
+        "location": "Location: Muhaisnah",
+        "price": 10.00,
+        "image":"images/gymnastics.jpg",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id":1005,
+        "title": "Karate",
+        "location": "Location: Mirdif",
+        "price": 10.00,
+        "image":"images/karate.png",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id":1005,
+        "title": "Music",
+        "location": "Location: Muhaisnah",
+        "price": 10.00,
+        "image":"images/music.jpg",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id":1005,
+        "title": "Soccer",
+        "location": "Location: Al Khail",
+        "price": 10.00,
+        "image":"images/soccer.jpg",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id":1005,
+        "title": "Tennis",
+        "location": "Location: Al Nahda",
+        "price": 10.00,
+        "image":"images/tennis.jpg",
+        "availableInventory": 5,
+        "rating": 2
+    },
+    {
+        "id":1005,
+        "title": "Theatre",
+        "location": "Location: Al Quoz",
+        "price": 10.00,
+        "image":"images/theatre.JPG",
+        "availableInventory": 5,
+        "rating": 2
+    },
+
+]
